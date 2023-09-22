c236540e0d1d5158e3e84c8c3e215042ecbf2cbc
fatihg61 <112856020+fatihg61@users.noreply.github.com>
2023-09-22 13:07:34 +0200

    push voor jalal

--- src/lib/slices/SquadMembers/index.svelte
+++ src/lib/slices/SquadMembers/index.svelte
@@ -10,34 +10,84 @@
   data-slice-variation={slice.variation}
 >
 <ul>
-{#each slice.items as item}
+  {#each slice.items as item}
     <li>
-      <h2>Naam: {item.memberdata.data.membername} </h2>
-      <h2>Squad: <span>{item.memberdata.data.squadname}</span></h2>
-      <span class="imgBorder"><PrismicImage field={item.memberdata.data.memberimg} width="140" height="200"/></span>
-      <a href="{item.memberdata.data.githublink.url}"><span>Github Link</span></a>
+      <div class="imgBorder">
+        <PrismicImage field={item.memberdata.data.memberimg} width="140" height="140"/>
+      </div>
+      <h2>Naam: {item.memberdata.data.membername}</h2>
+      <span>Squad: {item.memberdata.data.squadname}</span>
+      <a href="{item.memberdata.data.githublink.url}">Github Link</a>
     </li>
-
-{/each}
-
+  {/each}
 </ul>
 </section>
 
 <style>
-  ul{
+  /* Style for the profile cards */
+  ul {
+    list-style: none;
+    padding: 0;
     display: flex;
-    flex-flow: row wrap;
-    gap: 1.5em;
+    flex-wrap: wrap;
+    justify-content: center;
   }
-  li{
-    list-style-type: none;
-    width: 12em;
+
+  li {
+    background-color: #ffffff;
+    border: 1px solid #eaeaea;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    border-radius: 8px;
+    margin: 10px;
+    width: calc(33.33% - 20px);
     text-align: center;
+    transition: transform 0.3s ease-in-out;
   }
-  .imgBorder{
+
+  li:hover {
+    transform: translateY(-5px);
+  }
+
+  h2 {
+    font-size: 1.5rem;
+    margin: 10px 0;
+  }
+
+  span {
     display: block;
-    border: solid 4px rgb(110, 110, 110);
-    padding: 1em;
-    border-radius: 10px;
+    margin: 5px 0;
+  }
+
+  .imgBorder {
+    border-radius: 50%;
+    overflow: hidden;
+    width: 140px;
+    height: 140px;
+    margin: 0 auto;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+    display: block;
+    transition: transform 0.3s ease-in-out;
+  }
+
+  img:hover {
+    transform: scale(1.05);
+  }
+
+  a {
+    text-decoration: none;
+    color: #007bff;
+    display: block;
+    margin: 10px 0;
+  }
+
+  /* Responsive styles */
+  @media screen and (max-width: 768px) {
+    li {
+      width: 100%;
+    }
   }
 </style>
